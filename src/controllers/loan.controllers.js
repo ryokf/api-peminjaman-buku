07559f4d7f4f225db2527258181d5c16a7e845f3
ryokf <ryokhrisnaf@gmail.com>
2025-11-16 08:48:45 +0700

    feat: enhance loan creation and return logic with availability checks and transaction handling

--- src/controllers/loan.controllers.js
+++ src/controllers/loan.controllers.js
@@ -1,3 +1,4 @@
+import { da } from "@faker-js/faker";
 import { prisma } from "../prisma/client.js";
 import setReturnDate from "../utils/setReturnDate.js";
 
@@ -10,6 +11,40 @@ const getLoansByUser = async (req, res) => {
 }
 
 const createLoan = async (req, res) => {
+    const book = await prisma.book.findUnique({
+        where: { id: Number(req.body.book_id) },
+        select: {
+            isAvailable: true
+        }
+    });
+
+    if (!book || !book.isAvailable) {
+        return res.status(400).json({
+            message: "Book is not available for loan"
+        });
+    }
+
+    const borrower = await prisma.user.findUnique({
+        where: { id: Number(req.body.borrower_id) },
+        select: {
+            isBlacklist: true
+        }
+    });
+
+    if (borrower.isBlacklist) {
+        return res.status(400).json({
+            message: "Your account is blacklisted and cannot borrow books, please contact the library staff"
+        });
+    }
+
+    // Opsional: Cek batas maksimum peminjaman
+    const activeLoans = await prisma.loan.count({
+        where: { borrowerId: borrower.id, isDone: false }
+    });
+    if (activeLoans >= 2) { // Asumsi batas 3 buku
+        return res.status(400).json({ message: "You have reached maximum borrow limit" });
+    }
+
     try {
         const {
             book_id,
@@ -28,19 +63,29 @@ const createLoan = async (req, res) => {
         }
 
         const borrowDate = new Date()
-        const loan = await prisma.loan.create({
-            data: {
-                bookId: Number(book_id),
-                borrowerId: Number(borrower_id),
-                returnDate: new Date(setReturnDate(borrowDate)),
-                isDone: Boolean(is_done),
-                isLate: Boolean(is_late),
-                isDamaged: Boolean(is_damaged),
-                photo
-            },
-        });
+        const [loan, _] = await prisma.$transaction([
+            prisma.loan.create({
+                data: {
+                    bookId: Number(book_id),
+                    borrowerId: Number(borrower_id),
+                    returnDate: new Date(setReturnDate(borrowDate)),
+                    isDone: Boolean(is_done),
+                    isLate: Boolean(is_late),
+                    isDamaged: Boolean(is_damaged),
+                    photo
+                },
+            }),
+            prisma.book.update({
+                where: { id: Number(book_id) },
+                data: { isAvailable: false }
+            })
+        ]);
 
-        return res.status(201).json(loan);
+        return res.status(201).json({
+            status: 201,
+            message: "Loan created successfully",
+            data: loan
+        });
     } catch (error) {
         return res.status(500).json({ message: "Internal server error", error: error.message });
     }
@@ -49,16 +94,43 @@ const createLoan = async (req, res) => {
 const returnBook = async (req, res) => {
     try {
         const id = Number(req.params.id);
-        const { isDamaged, isLate } = req.body
+        const { isDamaged } = req.body
 
-        const loan = await prisma.loan.update({
+        const loanData = await prisma.loan.findUnique({
             where: { id },
-            data: {
-                isDone: true,
-                isDamaged,
-                isLate
+            select: {
+                returnDate: true
             }
-        })
+        });
+
+        let isLate= false
+        const currentDate = new Date();
+        if (currentDate > loanData.returnDate) {
+            isLate = true;
+        }
+
+        const [loan, _] = await prisma.$transaction([
+            prisma.loan.update({
+                where: { id },
+                data: {
+                    isDone: true,
+                    isDamaged,
+                    isLate
+                }
+            }),
+            prisma.book.updateMany({
+                where: {
+                    loans: {
+                        some: {
+                            id: id
+                        }
+                    }
+                },
+                data: {
+                    isAvailable: true
+                }
+            })
+        ]);
 
         res.status(200).json({
             status: 200,
